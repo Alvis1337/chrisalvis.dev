d9b5fcf00d7b9af6c788a9fa6bce32fc4c4e0c25
chrisalvis <alvisc@hocking.edu>
2024-03-11 00:44:37 -0400

    image compression, pwa support, webfont downloading locally. these are all just compiler optimizations.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import {Provider} from "react-redux";
 import {persistor, store} from "./store/store.ts";
 import {PersistGate} from "redux-persist/integration/react";
